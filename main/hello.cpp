6c63679e01855eae0b229a9ecb4bfa3eec1769cb
Gagan Vidyaranya <gaganvidyaranya123@gmail.com>
2023-02-17 10:31:31 +0530

    Chak de India

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "This is PES1ug20cs146 \n";
-  cout << "I have successfully built and run\n";
+  cout << "I have successfully built and run, chak de India \n";
   return 0;
 }
